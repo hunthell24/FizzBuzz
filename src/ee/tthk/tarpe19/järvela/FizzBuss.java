3eedb15b4411f03e89792c26b91ff8e9e4e793e0
opilane <opilane@HP-CZC6297B93.tthk.ee>
2021-10-27 13:01:27 +0300

    delete unnessosri fails

---
+++ src/ee/tthk/tarpe19/järvela/FizzBuss.java
@@ -0,0 +1,5 @@
+package ee.tthk.tarpe19.järvela;
+
+public class FizzBuss {
+
+}
---
+++ src/ee/tthk/tarpe19/järvela/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package ee.tthk.tarpe19.järvela;
+
+import junit.framework.TestCase;
+
+public class FizzBuzzTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
